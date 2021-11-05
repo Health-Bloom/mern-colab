8057dc908c9fd1748666769ec927bdda791cb747
Saptak10 <saptak.bugatti20@gmail.com>
2021-11-05 11:37:02 +0530

    pages

---
+++ frontend/src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeNav from './homenav.js';
+
+function Home() {
+    return (
+      <div>
+        <HomeNav />
+      </div>
+    );
+  }
+  
+  export default Home;
